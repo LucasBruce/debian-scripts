1b4fa88857cdd83aee68b3259b8095aba29cc445
LucasBruce <lucasmatheusbruce@gmail.com>
2025-10-28 14:10:42 -0300

    Update debian-nvm-install

--- debian-nvm-install.sh
+++ debian-nvm-install.sh
@@ -1,3 +1,1 @@
-curl -o- https://cdn.jsdelivr.net/gh/nvm-sh/nvm@v0.39.6/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"\n[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"\n
+curl -o- https://ghproxy.net/https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
